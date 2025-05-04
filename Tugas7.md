6534447507a8684918920a7fdd599e5c2f416477
haris49 <hariis.459@gmail.com>
2025-05-04 20:22:58 +0700

    middleware, gate admin

---
+++ Tugas7.md
@@ -0,0 +1,4 @@
+## Tugas 7
+
+Middleware & Gate Admin Not Active (![alt text](<Screenshots Tugas 7/Tugas7.md/Middleware & Gate Admin Not Active.png>))
+Middleware & Gate Admin Active (![alt text](<Screenshots Tugas 7/Tugas7.md/Middleware & Gate Admin Active.png>))
--- routes/web.php
+++ routes/web.php
@@ -14,31 +14,23 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    // Profile routes
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Todo routes
-    Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
-    Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
-    Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
-    Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
-    Route::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
+    
+    Route::resource('todo', TodoController::class)->except(['show']);
+    Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
     Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
     Route::patch('/todo/{todo}/incomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
-    Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
-    Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
+});
 
-    // User routes
-    Route::get('/user', [UserController::class, 'index'])->name('user.index');
-    Route::get('/user/{id}', [UserController::class, 'show'])->name('users.show');
-
-    // ✅ Tambahan baru: hapus user
-    Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
-
+// Admin-only user management
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::resource('user', UserController::class)->except(['create', 'store', 'edit', 'update']);
     Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
     Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
 });
 
-require __DIR__.'/auth.php'; 
+require __DIR__.'/auth.php';
